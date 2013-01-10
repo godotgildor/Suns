2ba315654d04571dd3cd0f2cdcdfb3814c8d9927
Brett T. Hannigan <bhannigan@palenque.degrado>
2013-01-09 16:20:16 -0800

    Removing explicit cancel search code and rolling it into the stop search code.  Also some minor code cleanup.  Still need to look at making fetch_full_context not block...

--- suns_search.py
+++ suns_search.py
@@ -9,13 +9,17 @@ import socket
 
 WORDS_DICT = {'lys_linker': {'LYS': 'CA+CB+CB+CG+CD+CG'}, 'ser_linker': {'SER': 'CA+CB'}, 'alanine': {'ALA': 'CA+CB'}, 'guanidinium': {'ARG': 'CD+NE+CZ+NH1+CZ+NE+CZ+NH2'}, 'glu_linker': {'GLU': 'CA+CB+CB+CG+CD+CG'}, 'valine': {'VAL': 'CB+CG2+CA+CB+CB+CG1'}, 'lys_end': {'LYS': 'CD+CE+CE+NZ'}, 'isoleucine': {'ILE': 'CD1+CG1+CB+CG1+CB+CG2+CA+CB'}, 'thr_linker': {'THR': 'CA+CB+CB+CG2'}, 'indole': {'TRP': 'CD1+CG+CD1+NE1+CD2+CG+CE2+NE1+CD2+CE3+CD2+CE2+CE3+CZ3+CE2+CZ2+CH2+CZ3+CH2+CZ2'}, 'asn_linker': {'ASN': 'CB+CG+CA+CB'}, 'trp_linker': {'TRP': 'CA+CB+CB+CG'}, 'arg_linker': {'ARG': 'CD+CG+CB+CG+CA+CB'}, 'leucine': {'LEU': 'CA+CB+CB+CG+CD2+CG+CD1+CG'}, 'phospho_thr': {'TPO': 'O2P+P+OG+P+O1P+P+O3P+P+O1P+O3P'}, 'phospho_his': {'HIP': 'O2P+P+OG+P+O1P+P+O3P+P+O1P+O3P'}, 'proline': {'PRO': 'CD+CG+CB+CG'}, 'imidazole': {'HIS': 'CD2+NE2+CE1+NE2+CD2+CG+CE1+ND1+CG+ND1'}, 'carboxyl': {'ASP': 'CG+OD2+CG+OD1', 'GLU': 'CD+OE2+CD+OE1'}, 'phospho_tyr': {'PTR': 'O2P+P+OG+P+O1P+P+O3P+P+O1P+O3P'}, 'phenyl': {'PHE': 'CD2+CE2+CD2+CG+CE2+CZ+CD1+CG+CE1+CZ+CD1+CE1', 'TYR': 'CE1+CZ+CD1+CE1+CE2+CZ+CD1+CG+CD2+CE2+CD2+CG'}, 'asp_linker': {'ASP': 'CB+CG+CA+CB'}, 'hydroxyl': {'SER': 'CB+OG', 'TYR': 'CZ+OH', 'THR': 'CB+OG1'}, 'gln_linker': {'GLN': 'CA+CB+CB+CG+CD+CG'}, 'phospho_asp': {'PHD': 'O2P+P+OG+P+O1P+P+O3P+P+O1P+O3P'}, 'phospho_ser': {'SEP': 'O2P+P+OG+P+O1P+P+O3P+P+O1P+O3P'}, 'tyr_linker': {'TYR': 'CA+CB+CB+CG'}, 'peptide_bond': {'CYS': 'CA+N+C+CA+C+O', 'GLN': 'CA+N+C+CA+C+O', 'HIS': 'CA+N+C+CA+C+O', 'SER': 'CA+N+C+CA+C+O', 'VAL': 'CA+N+C+CA+C+O', 'LYS': 'CA+N+C+CA+C+O', 'ILE': 'CA+N+C+CA+C+O', 'PRO': 'CA+N+C+CA+C+O', 'GLY': 'CA+N+C+CA+C+O', 'THR': 'CA+N+C+CA+C+O', 'PHE': 'CA+N+C+CA+C+O', 'ALA': 'CA+N+C+CA+C+O', 'MET': 'CA+N+C+CA+C+O', 'ASP': 'CA+N+C+CA+C+O', 'GLU': 'CA+N+C+CA+C+O', 'LEU': 'CA+N+C+CA+C+O', 'ARG': 'CA+N+C+CA+C+O', 'TRP': 'CA+N+C+CA+C+O', 'ASN': 'CA+N+C+CA+C+O', 'TYR': 'CA+N+C+CA+C+O'}, 'carboxamide': {'ASN': 'CG+ND2+CG+OD1', 'GLN': 'CD+NE2+CD+OE1'}, 'phe_linker': {'PHE': 'CA+CB+CB+CG'}, 'cysteine': {'CYS': 'CA+CB+CB+SG'}, 'met_linker': {'MET': 'CA+CB+CB+CG'}, 'his_linker': {'HIS': 'CB+CG+CA+CB'}, 'met_end': {'MET': 'CE+SD+CG+SD'}}
 BOND_WORD_DICT = {('PRO', 'CB', 'CG'): 'proline', ('ILE', 'C', 'O'): 'peptide_bond', ('TRP', 'CE2', 'NE1'): 'indole', ('ARG', 'CZ', 'NH1'): 'guanidinium', ('LYS', 'C', 'CA'): 'peptide_bond', ('TPO', 'O2P', 'P'): 'phospho_thr', ('HIP', 'O3P', 'P'): 'phospho_his', ('GLY', 'CA', 'N'): 'peptide_bond', ('ARG', 'CZ', 'NE'): 'guanidinium', ('ARG', 'C', 'CA'): 'peptide_bond', ('TYR', 'CA', 'N'): 'peptide_bond', ('TYR', 'CD2', 'CG'): 'phenyl', ('VAL', 'C', 'CA'): 'peptide_bond', ('SEP', 'O2P', 'P'): 'phospho_ser', ('TPO', 'O1P', 'O3P'): 'phospho_thr', ('GLY', 'C', 'CA'): 'peptide_bond', ('GLU', 'CD', 'CG'): 'glu_linker', ('TRP', 'CD1', 'NE1'): 'indole', ('TRP', 'CD2', 'CE2'): 'indole', ('ASP', 'C', 'CA'): 'peptide_bond', ('ILE', 'CB', 'CG1'): 'isoleucine', ('ASN', 'C', 'CA'): 'peptide_bond', ('MET', 'CB', 'CG'): 'met_linker', ('HIP', 'O2P', 'P'): 'phospho_his', ('PHE', 'CE1', 'CZ'): 'phenyl', ('PRO', 'C', 'CA'): 'peptide_bond', ('PHE', 'CE2', 'CZ'): 'phenyl', ('TYR', 'CA', 'CB'): 'tyr_linker', ('PHE', 'C', 'O'): 'peptide_bond', ('GLN', 'CD', 'NE2'): 'carboxamide', ('HIS', 'CG', 'ND1'): 'imidazole', ('SEP', 'O1P', 'P'): 'phospho_ser', ('PHE', 'CD1', 'CE1'): 'phenyl', ('PTR', 'O1P', 'O3P'): 'phospho_tyr', ('SEP', 'OG', 'P'): 'phospho_ser', ('ALA', 'CA', 'N'): 'peptide_bond', ('LEU', 'C', 'CA'): 'peptide_bond', ('THR', 'CB', 'OG1'): 'hydroxyl', ('ASP', 'CG', 'OD1'): 'carboxyl', ('PHD', 'O3P', 'P'): 'phospho_asp', ('LYS', 'CA', 'CB'): 'lys_linker', ('MET', 'C', 'O'): 'peptide_bond', ('TRP', 'CD2', 'CG'): 'indole', ('HIS', 'CB', 'CG'): 'his_linker', ('MET', 'CG', 'SD'): 'met_end', ('ARG', 'C', 'O'): 'peptide_bond', ('TRP', 'CA', 'CB'): 'trp_linker', ('ARG', 'CB', 'CG'): 'arg_linker', ('PTR', 'O3P', 'P'): 'phospho_tyr', ('LEU', 'CD1', 'CG'): 'leucine', ('SEP', 'O3P', 'P'): 'phospho_ser', ('MET', 'CA', 'CB'): 'met_linker', ('TYR', 'CD1', 'CE1'): 'phenyl', ('ALA', 'CA', 'CB'): 'alanine', ('GLU', 'CD', 'OE1'): 'carboxyl', ('PHE', 'CD1', 'CG'): 'phenyl', ('THR', 'CA', 'N'): 'peptide_bond', ('TYR', 'CZ', 'OH'): 'hydroxyl', ('ASP', 'CA', 'CB'): 'asp_linker', ('THR', 'CA', 'CB'): 'thr_linker', ('TRP', 'CA', 'N'): 'peptide_bond', ('SER', 'C', 'CA'): 'peptide_bond', ('ASP', 'CA', 'N'): 'peptide_bond', ('MET', 'CA', 'N'): 'peptide_bond', ('TRP', 'CH2', 'CZ2'): 'indole', ('ILE', 'CA', 'CB'): 'isoleucine', ('LYS', 'CD', 'CG'): 'lys_linker', ('PRO', 'CD', 'CG'): 'proline', ('GLU', 'C', 'O'): 'peptide_bond', ('TRP', 'CH2', 'CZ3'): 'indole', ('TPO', 'O1P', 'P'): 'phospho_thr', ('ILE', 'CA', 'N'): 'peptide_bond', ('CYS', 'C', 'CA'): 'peptide_bond', ('LYS', 'CD', 'CE'): 'lys_end', ('GLY', 'C', 'O'): 'peptide_bond', ('CYS', 'CA', 'CB'): 'cysteine', ('MET', 'C', 'CA'): 'peptide_bond', ('VAL', 'CA', 'N'): 'peptide_bond', ('THR', 'CB', 'CG2'): 'thr_linker', ('LEU', 'CA', 'CB'): 'leucine', ('ASP', 'C', 'O'): 'peptide_bond', ('LEU', 'CB', 'CG'): 'leucine', ('PRO', 'C', 'O'): 'peptide_bond', ('PHD', 'O1P', 'P'): 'phospho_asp', ('PHE', 'C', 'CA'): 'peptide_bond', ('ARG', 'CZ', 'NH2'): 'guanidinium', ('TYR', 'CB', 'CG'): 'tyr_linker', ('TYR', 'CE1', 'CZ'): 'phenyl', ('TYR', 'CE2', 'CZ'): 'phenyl', ('VAL', 'CB', 'CG2'): 'valine', ('LEU', 'C', 'O'): 'peptide_bond', ('LEU', 'CA', 'N'): 'peptide_bond', ('VAL', 'CA', 'CB'): 'valine', ('GLU', 'CA', 'N'): 'peptide_bond', ('LYS', 'C', 'O'): 'peptide_bond', ('SER', 'C', 'O'): 'peptide_bond', ('PTR', 'O2P', 'P'): 'phospho_tyr', ('GLU', 'CA', 'CB'): 'glu_linker', ('GLU', 'CB', 'CG'): 'glu_linker', ('GLN', 'CA', 'N'): 'peptide_bond', ('PHE', 'CD2', 'CG'): 'phenyl', ('TYR', 'C', 'CA'): 'peptide_bond', ('HIS', 'CA', 'CB'): 'his_linker', ('PTR', 'O1P', 'P'): 'phospho_tyr', ('TRP', 'CE2', 'CZ2'): 'indole', ('ASN', 'CG', 'OD1'): 'carboxamide', ('HIS', 'CA', 'N'): 'peptide_bond', ('ILE', 'CB', 'CG2'): 'isoleucine', ('HIS', 'CD2', 'NE2'): 'imidazole', ('TRP', 'CD2', 'CE3'): 'indole', ('ALA', 'C', 'CA'): 'peptide_bond', ('LEU', 'CD2', 'CG'): 'leucine', ('TRP', 'C', 'CA'): 'peptide_bond', ('GLN', 'CA', 'CB'): 'gln_linker', ('GLN', 'CB', 'CG'): 'gln_linker', ('HIP', 'OG', 'P'): 'phospho_his', ('THR', 'C', 'O'): 'peptide_bond', ('HIS', 'C', 'O'): 'peptide_bond', ('TRP', 'CD1', 'CG'): 'indole', ('ILE', 'C', 'CA'): 'peptide_bond', ('SER', 'CA', 'N'): 'peptide_bond', ('GLU', 'C', 'CA'): 'peptide_bond', ('GLN', 'C', 'CA'): 'peptide_bond', ('SEP', 'O1P', 'O3P'): 'phospho_ser', ('CYS', 'C', 'O'): 'peptide_bond', ('SER', 'CB', 'OG'): 'hydroxyl', ('HIS', 'CE1', 'NE2'): 'imidazole', ('PHE', 'CA', 'N'): 'peptide_bond', ('HIP', 'O1P', 'P'): 'phospho_his', ('ASP', 'CG', 'OD2'): 'carboxyl', ('ASN', 'CB', 'CG'): 'asn_linker', ('LYS', 'CE', 'NZ'): 'lys_end', ('CYS', 'CB', 'SG'): 'cysteine', ('VAL', 'CB', 'CG1'): 'valine', ('TYR', 'C', 'O'): 'peptide_bond', ('TPO', 'OG', 'P'): 'phospho_thr', ('VAL', 'C', 'O'): 'peptide_bond', ('HIP', 'O1P', 'O3P'): 'phospho_his', ('ARG', 'CD', 'NE'): 'guanidinium', ('ALA', 'C', 'O'): 'peptide_bond', ('LYS', 'CB', 'CG'): 'lys_linker', ('GLU', 'CD', 'OE2'): 'carboxyl', ('TPO', 'O3P', 'P'): 'phospho_thr', ('PHD', 'O2P', 'P'): 'phospho_asp', ('PHE', 'CA', 'CB'): 'phe_linker', ('ASN', 'CG', 'ND2'): 'carboxamide', ('HIS', 'CD2', 'CG'): 'imidazole', ('PHE', 'CD2', 'CE2'): 'phenyl', ('PRO', 'CA', 'N'): 'peptide_bond', ('ASN', 'C', 'O'): 'peptide_bond', ('ARG', 'CD', 'CG'): 'arg_linker', ('ARG', 'CA', 'N'): 'peptide_bond', ('TRP', 'CB', 'CG'): 'trp_linker', ('PHE', 'CB', 'CG'): 'phe_linker', ('HIS', 'C', 'CA'): 'peptide_bond', ('PHD', 'OG', 'P'): 'phospho_asp', ('MET', 'CE', 'SD'): 'met_end', ('GLN', 'C', 'O'): 'peptide_bond', ('HIS', 'CE1', 'ND1'): 'imidazole', ('CYS', 'CA', 'N'): 'peptide_bond', ('TRP', 'CE3', 'CZ3'): 'indole', ('TYR', 'CD2', 'CE2'): 'phenyl', ('ILE', 'CD1', 'CG1'): 'isoleucine', ('PTR', 'OG', 'P'): 'phospho_tyr', ('TRP', 'C', 'O'): 'peptide_bond', ('LYS', 'CA', 'N'): 'peptide_bond', ('TYR', 'CD1', 'CG'): 'phenyl', ('GLN', 'CD', 'CG'): 'gln_linker', ('ARG', 'CA', 'CB'): 'arg_linker', ('ASN', 'CA', 'N'): 'peptide_bond', ('SER', 'CA', 'CB'): 'ser_linker', ('GLN', 'CD', 'OE1'): 'carboxamide', ('PHD', 'O1P', 'O3P'): 'phospho_asp', ('THR', 'C', 'CA'): 'peptide_bond', ('ASN', 'CA', 'CB'): 'asn_linker', ('ASP', 'CB', 'CG'): 'asp_linker'}
-SELECTION_NAME = 'query'
+SELECTION_NAME = 'suns_query'
 OBJECT_SUFFIX = 'suns'
 
 SUNS_SERVER_ADDRESS = 'suns.degradolab.org'
 
 class SearchThread(threading.Thread):
     def __init__(self, rmsd, num_structures, random_seed, pdbstrs, server_address, cmd):
+        '''
+        This is the constructor for our SearchThread.  Each time we perform
+        a structural search, a new thread will be created.
+        '''
         threading.Thread.__init__(self)
         self.request = json.dumps({
             'rmsd_cutoff'   : rmsd,
@@ -28,11 +32,19 @@ class SearchThread(threading.Thread):
         self.callback_queue = None
         self.pdbs = {}
         self.suns_server_address = server_address
-        self.begun = False
-        self.ended = False
-        self.lock = threading.Lock()
+        self.loaded_objects = []
+        # The following variable will ensure that we don't
+        # stop consuming before we begin consuming etc.
+        self.concurency_management = {'begun': False,
+                                      'ended' : False,
+                                      'lock' : threading.Lock()}
     
     def handle_delivery(self, channel, method_frame, header_frame, body, corr_id=None):
+        '''
+        This method will handle delivery from the message queue we use to communicate
+        with the Suns server.
+        '''
+        prev_existing_objects = dict([(obj_name, 1) for obj_name in self.cmd.get_names()])
         if(corr_id == header_frame.correlation_id):
             # TODO Consider changing to JSON in the future.
             # The first byte is the status.
@@ -48,14 +60,18 @@ class SearchThread(threading.Thread):
                 pdbid = body[1:5]
                 if(pdbid not in self.pdbs):
                     self.pdbs[pdbid] = 0
+                # The new object name will be of the form pdbid_XXXX_suffix
+                # XXXX is an integer.  We should check to see if there is already
+                # a selection with that name, and if so, bump up the integer count
+                # until we get a new unique name.  Otherwise pymol will load the 
+                # new pdb info as a second model in the existing object.
                 sele_name = pdbid + '_%04d_%s' % (self.pdbs[pdbid], OBJECT_SUFFIX)
-                # Delete this object if it already exists for some reason
-                #while exists sele_name:
-                #    self.pdbs[pdbid] += 1
-                #    sele_name = pdbid + '_%04d_%s' % (self.pdbs[pdbid], OBJECT_SUFFIX)
-                self.cmd.delete(sele_name)
+                while sele_name in prev_existing_objects:
+                    self.pdbs[pdbid] += 1
+                    sele_name = pdbid + '_%04d_%s' % (self.pdbs[pdbid], OBJECT_SUFFIX)
                 # Load the structure into pymol.
                 self.cmd.read_pdbstr(body[5:], sele_name)
+                self.loaded_objects += [sele_name]
                 # Increment the number of results for this pdbid
                 self.pdbs[pdbid] += 1
             elif(body[0] == '2'):
@@ -65,14 +81,21 @@ class SearchThread(threading.Thread):
                 print '[*] Error: ' + body[1:]
     
     def delete_current_results(self, exceptions = {}):
-        for pdbid in self.pdbs:
-            for i in range(self.pdbs[pdbid]):
-                sele_name = pdbid + '_%04d_%s' % (i, OBJECT_SUFFIX)
-                if(sele_name not in exceptions):
-                    self.cmd.delete(sele_name)
+        '''
+        This method will delete all of the objects that have been loaded as a result
+        of this query, with the exception of any objects listed in the exceptions variable.
+        '''
+        for sele_name in self.loaded_objects:
+            if(sele_name not in exceptions):
+                self.cmd.delete(sele_name)
     
     # Perform our search.
     def run(self):
+        '''
+        This method will send the search request to the server and tell Pika about the callback
+        function to use for results returned from the server.  It will actuallly block until 
+        we call stop_consuming, which will occur in the callback.
+        '''
         corr_id = str(uuid.uuid4())
         credentials = PlainCredentials('suns-client', 'suns-client')
         try:
@@ -97,14 +120,20 @@ class SearchThread(threading.Thread):
                                           properties=BasicProperties(reply_to = self.callback_queue, correlation_id = corr_id ),
                                           body = self.request)
                     
-                    # This call is a blocking call.  It will block until channel.stop_consuming is called.
                     try:
-                        self.lock.acquire()
+                        # We are going to change our state from search not begun
+                        # to search begun.  This needs to be atomic, so that if
+                        # the user tries to cancel the search, the state is accurate.
+                        # We can't put the start_consuming inside the lock however
+                        # since start_consuming is a blocking call, and thus we wouldn't
+                        # unlock until after we have stopped consuming, thus negating
+                        # the whole point of the cancel search in the first place.
+                        self.concurency_management['lock'].acquire()
                         try:
-                            self.begun = True
-                            ended = self.ended
+                            self.concurency_management['begun'] = True
+                            ended = self.concurency_management['ended']
                         finally:
-                            self.lock.release()
+                            self.concurency_management['lock'].release()
                         # Race condition:
                         #     This thread could receive a stop() signal before
                         #     start_consuming or could receive a second
@@ -115,11 +144,13 @@ class SearchThread(threading.Thread):
                         if (not ended):
                             self.channel.start_consuming()
                     finally:
-                        self.lock.acquire()
+                        # We are not consuming data anymore, since start_consuming
+                        # is no longer blocking, so set the ended flag.
+                        self.concurency_management['lock'].acquire()
                         try:
-                            self.ended = True
+                            self.concurency_management['ended'] = True
                         finally:
-                            self.lock.release()
+                            self.concurency_management['lock'].release()
                         self.cmd.orient(SELECTION_NAME)
                 finally:
                     self.channel.queue_delete(queue=self.callback_queue)
@@ -128,19 +159,24 @@ class SearchThread(threading.Thread):
         except socket.error:
             print "[*] Error: Unable to connect to '" + self.suns_server_address + "'"
     
-    def cancel_search(self):
-        print '[*] Search cancelled'
-        self.stop()
-    
-    def stop(self):
-        self.lock.acquire()
+    def stop(self, message=''):
+        '''
+        This method will see if we have begun consuming, but haven't
+        ended yet.  If so, then it will stop_consuming, thus completing
+        the search.
+        '''
+        # Since we are going to change the state of our consuming,
+        # we need to acquire a lock.
+        self.concurency_management['lock'].acquire()
         try:
-            if(not self.ended):
-                if(self.begun):
+            if(not self.concurency_management['ended']):
+                if(self.concurency_management['begun']):
                     self.channel.stop_consuming()
-                self.ended = True
+                self.concurency_management['ended'] = True
         finally:
-            self.lock.release()
+            self.concurency_management['lock'].release()
+            if(message != ''):
+                print message
 
 class Suns_search(Wizard):
     '''
@@ -300,7 +336,7 @@ class Suns_search(Wizard):
         return [
             [ 1, 'Structural Search Engine',''],
             [ 2, 'Search', 'cmd.get_wizard().launch_search()'],
-            [ 2, 'Cancel Search','cmd.get_wizard().cancel_search()'],
+            [ 2, 'Cancel Search','cmd.get_wizard().stop("[*] Search cancelled")'],
             [ 3, 'RMSD Cutoff: ' + str(self.rmsd_cutoff) + ' Angstroms', 'rmsd'],
             [ 3, 'Cap: ' + str(self.number_of_structures) + ' results', 'num_structures'],
             [ 3, 'Order: ' + {True: 'Random (Seed = %d' % self.random_seed + ')', False: 'Default'}[self.random_seed != 0], 'random_seed'],
@@ -308,7 +344,7 @@ class Suns_search(Wizard):
             [ 2, 'Clear Results', 'cmd.get_wizard().delete_current_results()'],
             [ 2, 'Clear Selection','cmd.get_wizard().clear_selection()'],
             [ 2, 'Fetch Full Context','cmd.get_wizard().fetch_full_context()'],
-# There should be a cancel for Fetch Full Context
+            # TODO There should be a cancel for Fetch Full Context
             [ 2, 'Done','cmd.set_wizard()'] ]
     
     def fetch_full_context(self):
@@ -323,9 +359,9 @@ class Suns_search(Wizard):
         objs = self.cmd.get_names('objects', enabled_only=1)
         for obj in objs:
             w = obj.split('_')
-            # Note that the search results will be named pdbid_XXXX.
+            # Note that the search results will be named pdbid_XXXX_suffix.
             if( (len(w) == 3) and (len(w[0]) == 4) and (len(w[1]) == 4)):
-                new_object_name = w[0] + '_full_' + w[1]
+                new_object_name = w[0] + '_full_' + w[1] + '_' + w[2]
                 self.cmd.fetch(w[0], new_object_name)
                 dict = {'x' : []}
                 # Get the atom info for the current object.
@@ -379,13 +415,9 @@ class Suns_search(Wizard):
         self.word_list = {}
         self.cmd.delete(SELECTION_NAME)
     
-    def cancel_search(self):
+    def stop_search(self, message = ''):
         if(self.searchThread is not None):
-            self.searchThread.cancel_search()
-
-    def stop_search(self):
-        if(self.searchThread is not None):
-            self.searchThread.stop()
+            self.searchThread.stop(message)
     
     def do_select(self, name, selection_logic):
         if(selection_logic != ''):
